36667f4f285e518509dc6440152203dd1188b051
Max Trofimov <qwerty65k@mail.ru>
2024-12-08 21:16:47 +0300

    fix hints

--- files/.config/nvim/lua/configs/colors.lua
+++ files/.config/nvim/lua/configs/colors.lua
@@ -1,5 +1,5 @@
 vim.opt.termguicolors = true
-vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#8470ba", bold=true})
+vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#404040", bold=false})
 vim.api.nvim_set_hl(0, "Comment", { fg = "#767676"})
 vim.api.nvim_set_hl(0, "@comment", { link = "Comment"})
 
